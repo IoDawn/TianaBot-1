6facd8c73e9789e9dd701abd46825b51e16974d9
ᴀʀᴜɴᴀ <75518008+IoDawn@users.noreply.github.com>
2021-07-31 06:23:23 +0700

    Update musicplayer.py

--- Tianabot/modules/musicplayer.py
+++ Tianabot/modules/musicplayer.py
@@ -1,10 +1,8 @@
-__mod_name__ = "VC MUSIC"
+__mod_name__ = "Music"
 __help__ = """
- ❍ /search <query> : Search videos on youtube with details.
  ❍ /song <song name> : Download songs you want quickly.
  ❍ /deezer <song name> : Download songs you want quickly via deezer.
  ❍ /saavn <song name> : Download songs you want quickly via saavn.
  ❍ /video <video name> : Download videos you want quickly.
- ❍ /vsong <song name> : Download video songs.
- ❍ /vchelp : Do this to open VC help module."""
+ ❍ /vsong <song name> : Download video songs."""
 
